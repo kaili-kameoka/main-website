6f048bec61a040d7bcf72816e253cc021a407d29
Kaili Kameoka <kaili.kameoka@gmail.com>
2024-08-17 15:02:16 -1000

    Fixed Header

--- src/library/components/Header/Header.tsx
+++ src/library/components/Header/Header.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import './Header.scss'
 import {MAIN_NAV_MENU_ITEMS, SITE_TITLE} from "@/library/utils/content";
 import Navigation from "@/library/components/Navigation/Navigation.tsx";
+import Link from "next/link";
 
 function Header() {
 
 	return (
 			<header className="Header">
 				<div className="Header__Container">
-					<h1 className='Header__Title'>{SITE_TITLE}</h1>
+					<h1 className='Header__Title'>
+						<Link href={'/'}>{SITE_TITLE}</Link>
+					</h1>
 					<Navigation menuItems={MAIN_NAV_MENU_ITEMS} />
 				</div>
 			</header>
